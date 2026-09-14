5919354cfb0a62859be70fbacd2b15c54adc4d8d
educonnect-1 <educonnect105@gmail.com>
2026-09-14 18:27:49 +0300

    Delete marwan-elgendi-academy app directory

--- marwan-elgendi-academy app/app/lib/features/announcements/application/announcements_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../../models/announcement.dart';
-import '../../../repositories/announcement_repository.dart';
-
-final myAnnouncementsProvider = FutureProvider.autoDispose<List<Announcement>>((ref) {
-  return ref.read(announcementRepositoryProvider).getMyAnnouncements();
-});
